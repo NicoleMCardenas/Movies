0094b851f33ad942b95372d01270ef407cda6438
nicolemcardenas <nicmocard@gmail.com>
2025-06-24 13:07:50 -0600

    avanceAxios

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,4 +1,11 @@
+const axios = require('axios')
+
 const renderTarjeta = require("./tarjeta")
-    $.get("https://students-api.up.railway.app/movies",(data)=>{
-        renderTarjeta(data);
-        });
+       
+    axios.get('https://students-api.up.railway.app/movies')
+        .then((info) => {
+              renderTarjeta(info.data)
+        })
+        .catch(() =>{
+            alert('No se cargaron las películas, error del servidor')
+        })
